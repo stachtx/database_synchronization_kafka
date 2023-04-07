058cda230bd7ef57209800091e3a0c004cbe8061
Tomasz Stachura <tom-stachura@outlook.com>
2023-04-07 18:34:15 +0200

    Setting liquibase for tests

--- central_server/src/test/java/com/database/integration/central/controllers/DepartmentControllerTest.java
+++ central_server/src/test/java/com/database/integration/central/controllers/DepartmentControllerTest.java
@@ -1,10 +1,13 @@
 package com.database.integration.central.controllers;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class DepartmentControllerTest {
 
 
